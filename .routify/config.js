5e322e6945cb0dc5bfd76de0566b4eea7122d1ef
elianiva <dicha.arkana03@gmail.com>
2020-09-20 10:04:54 +0700

    chore: forgot to commit this

---
+++ .routify/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "pages": "src/pages",
+  "sourceDir": "public",
+  "routifyDir": ".routify",
+  "ignore": "",
+  "dynamicImports": true,
+  "singleBuild": false,
+  "noHashScroll": false,
+  "extensions": [
+    "svelte",
+    "html",
+    "svx",
+    "md"
+  ],
+  "distDir": "dist",
+  "hashScroll": true,
+  "started": "2020-09-20T02:57:52.542Z"
+}

7ae24d6ff57812610e348c609fbe43adef976c84
elianiva <dicha.arkana03@gmail.com>
2020-09-20 11:38:37 +0700

    fix: removed global.css causing several issues

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,6 @@
+<Router routes="{routes}" />
+
 <script>
-  import { Router } from "@sveltech/routify";
-  import { routes } from "../.routify/routes";
+import { Router } from "@sveltech/routify"
+import { routes } from "../.routify/routes"
 </script>
-
-<style  global>
-  @import "../static/global.css";
-</style>
-
-<Router {routes} />

4f856db1f932f74f66a40e9208ca227d64741fe8
elianiva <dicha.arkana03@gmail.com>
2020-09-20 09:55:50 +0700

    feat: initial routify migration

---
+++ src/pages/index.svelte
@@ -0,0 +1,8 @@
+<script>
+    import RoutifyIntro from './example/_components/RoutifyIntro.svelte'
+    import { metatags } from '@sveltech/routify'
+    metatags.title = 'Home | Three of Something'
+    metatags.description = 'Website to keep all of the result of a small challenge that I did with my friend.'
+</script>
+
+<RoutifyIntro />

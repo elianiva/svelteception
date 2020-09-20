fa702f7505c5dbf06f853fd31cfac7e3ee8b265f
elianiva <dicha.arkana03@gmail.com>
2020-09-20 10:45:40 +0700

    feat: added About component

---
+++ src/pages/_components/About.svelte
@@ -0,0 +1,58 @@
+<style>
+.about {
+  margin-bottom: 4rem;
+}
+
+.about__heading {
+  font-size: calc(1vw + 1.25rem);
+  font-weight: 800;
+  text-align: center;
+  color: #b0bec5;
+  font-family: "Fira Code", monospace;
+  margin-bottom: 2rem;
+}
+
+.about__desc {
+  background-color: #323232;
+  padding: 2rem;
+  border-radius: 0.5rem;
+  max-width: 80ch;
+  margin: 0 auto;
+  font-size: 1.125rem;
+  line-height: 2rem;
+  color: #b0bec5;
+  font-family: "PT Sans", sans-serif;
+}
+
+.about__desc a {
+  color: #82aaff;
+}
+
+.highlight {
+  color: #f4f4f4;
+}
+</style>
+
+<section class="about">
+  <h2 class="about__heading">About</h2>
+  <div class="about__desc">
+    <p>
+      What is this? Well, me and <a
+        href="https://nikarashihatsu.github.io/three-of-everything/"
+      >my friend</a> are doing this challenge where we try to implement 3 designs
+      each week into code. Feel free to submit your design to us btw. <br /> Why
+      is it called <span class="highlight">"Three of
+        &lbrace;Some,Every&rbrace;thing"</span>? It came up by accident
+      actually. The first time I make the repo for this challenge on github, I
+      don't know what to name it. So I was thinking, because we implement 3
+      designs each week, I want the word "three" for its name. Being me who
+      isn't really good at naming stuff so I just went with <span
+        class="highlight"
+      >Three of Something</span>. Then a few moments later, my friend made his
+      repo called <span class="highlight">Three of Everything</span>. If we
+      combine those, we'll get <span class="highlight">Three of
+        &lbrace;Some,Every&rbrace;thing</span>. And yes, that's BASH expansion
+      braces.
+    </p>
+  </div>
+</section>

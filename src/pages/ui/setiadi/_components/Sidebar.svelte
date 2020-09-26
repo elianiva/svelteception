e83905f429a14828bc70fc5a0ff66b91ea3c20bf
elianiva <dicha.arkana03@gmail.com>
2020-09-26 17:11:56 +0700

    feat: added sidebar component

---
+++ src/pages/ui/setiadi/_components/Sidebar.svelte
@@ -0,0 +1,56 @@
+<style>
+.sidebar {
+  position: fixed;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  width: 6rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.5rem;
+  background-color: #f7444e;
+}
+
+.sidebar__icon {
+  color: #ffffff;
+}
+
+.sidebar__icon:last-child {
+  position: absolute;
+  bottom: 2rem;
+}
+
+.sidebar__icon.active {
+  padding: 1rem;
+  color: #f7444e;
+  background-color: #ffffff;
+  border-radius: 1rem;
+}
+</style>
+
+<aside class="sidebar">
+  <div class="sidebar__icon">
+    <HomeIcon width="1.5rem" height="1.5rem" />
+  </div>
+  <div class="sidebar__icon active">
+    <BookIcon width="1.5rem" height="1.5rem" />
+  </div>
+  <div class="sidebar__icon">
+    <ChartIcon width="1.5rem" height="1.5rem" />
+  </div>
+  <div class="sidebar__icon">
+    <BellIcon width="1.5rem" height="1.5rem" />
+  </div>
+  <div class="sidebar__icon">
+    <SettingsIcon width="1.5rem" height="1.5rem" />
+  </div>
+</aside>
+
+<script>
+import HomeIcon from "../icons/house-icon.svg"
+import BookIcon from "../icons/book-icon.svg"
+import ChartIcon from "../icons/chart-icon.svg"
+import BellIcon from "../icons/bell-icon.svg"
+import SettingsIcon from "../icons/settings-icon.svg"
+</script>

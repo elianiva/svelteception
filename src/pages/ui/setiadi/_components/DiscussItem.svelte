95ab528147a50f5da9f81b785d82ad99e4abfc93
elianiva <dicha.arkana03@gmail.com>
2020-09-26 18:50:24 +0700

    feat: added discuss component

---
+++ src/pages/ui/setiadi/_components/DiscussItem.svelte
@@ -0,0 +1,50 @@
+<style>
+.discuss {
+  padding: 0.5rem 1rem;
+  font-family: "Roboto", sans-serif;
+}
+
+.discuss__profile {
+  display: grid;
+  grid-template-columns: 4rem 1fr;
+  grid-template-rows: 1fr 1fr;
+  column-gap: 1rem;
+  align-items: center;
+}
+
+.discuss__pict {
+  width: 4rem;
+  height: 4rem;
+  border-radius: 2rem;
+  grid-row: 1/3;
+  object-fit: cover;
+}
+
+.discuss__name {
+  font-weight: 600;
+  font-size: 1.125rem;
+}
+
+.discuss__date {
+  color: #787878;
+}
+
+.discuss__content {
+  margin-top: 0.5rem;
+  font-weight: 500;
+  color: #787878;
+}
+</style>
+
+<div class="discuss">
+  <div class="discuss__profile">
+    <img class="discuss__pict" src="{src}" alt="{name}" />
+    <span class="discuss__name">{name}</span>
+    <span class="discuss__date">{day} at {time} {period}</span>
+  </div>
+  <p class="discuss__content">{content}</p>
+</div>
+
+<script>
+export let src, name, day, time, period, content
+</script>

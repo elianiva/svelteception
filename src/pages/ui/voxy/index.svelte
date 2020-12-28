f61b9b9bf5f1485c4ccc7e4cc7276493ffdf226f
elianiva <dicha.arkana03@gmail.com>
2020-12-28 23:07:29 +0700

    chore: metatags

--- src/pages/ui/voxy/index.svelte
+++ src/pages/ui/voxy/index.svelte
@@ -1,5 +1,9 @@
 <script>
 import { goto } from "@roxi/routify"
-
 $goto("/ui/voxy/home")
+
+import { metatags } from "@roxi/routify"
+
+metatags.title = "Voxy | Svelteception"
+metatags.description = "Dataku UI by Voxy"
 </script>
